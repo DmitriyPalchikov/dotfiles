cacb8371d32141b4e76dd5430d268394d0b3c82b
Дмитрий Пальчиков <dpalchikov@moysklad.ru>
2026-01-15 14:23:09 +0300

    Change diagnostic info

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,71 +1,93 @@
--- ~/.config/nvim/lua/configs/lspconfig.lua
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
-local util = require "lspconfig.util"
+local util = require("lspconfig.util")
+local lspconfig = require("lspconfig")
 
--- Новый API - замените require("lspconfig").setup()
--- require("lspconfig").setup()  -- УДАЛИТЕ ЭТУ СТРОКУ
-
--- Список серверов
-local servers = {
-  "lua_ls",
-  "pyright",
-  "bashls",
-  "yamlls",
-  "dockerls",
+-- list of all servers configured.
+lspconfig.servers = {
+	"lua_ls",
+	"pyright",
+	"bashls",
+	"yamlls",
+	"dockerls",
 }
 
--- Настройка серверов с дефолтной конфигурацией
-local default_servers = { "pyright", "yamlls", "dockerls" }
+-- list of servers configured with default config.
+local default_servers = { "pyright" }
+
+-- lsps with default config
 for _, lsp in ipairs(default_servers) do
-  vim.lsp.config(lsp, {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  })
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		on_init = on_init,
+		capabilities = capabilities,
+	})
 end
 
--- lua_ls с кастомными настройками
-vim.lsp.config("lua_ls", {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        enable = false,
-      },
-      workspace = {
-        library = {
-          vim.fn.expand "$VIMRUNTIME/lua",
-          vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-          "${3rd}/love2d/library",
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
-    },
-  },
+lspconfig.lua_ls.setup({
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
+
+	settings = {
+		Lua = {
+			diagnostics = {
+				enable = false, -- Disable all diagnostics from lua_ls
+				-- globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					vim.fn.expand("$VIMRUNTIME/lua"),
+					vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+					vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+					vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+					"${3rd}/love2d/library",
+				},
+				maxPreload = 100000,
+				preloadFileSize = 10000,
+			},
+		},
+	},
 })
 
--- bashls с кастомными настройками
-vim.lsp.config("bashls", {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "bash-language-server", "start" },
-  filetypes = { "sh", "zsh", "bash" },
-  root_dir = util.root_pattern(".git", vim.fn.getcwd()),
-  settings = {
-    bash = {
-      diagnostics = {
-        enable = true,
-      },
-    },
-  },
+lspconfig.bashls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "bash-language-server", "start" },
+	filetypes = { "sh", "zsh", "bash" },
+	root_dir = util.root_pattern(".git", vim.fn.getcwd()),
+	settings = {
+		bash = {
+			diagnostics = {
+				enable = true,
+			},
+		},
+	},
 })
 
--- Активируем все LSP серверы (добавьте это в init.lua или после конфигурации)
-vim.lsp.enable(servers)
+-- === DIAGNOSTIC CONFIGURATION ===
+-- Явная конфигурация отображения ошибок для унификации между системами
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "●", -- или можно использовать "■", "▎", "x"
+		source = "if_many", -- показывать источник только если несколько источников
+		spacing = 4, -- расстояние между ошибкой и текстом
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true, -- сортировать по серьёзности
+	float = {
+		source = "always", -- показывать источник (pyright, luacheck и т.д.)
+		border = "rounded",
+		focusable = true,
+	},
+})
+
+-- Если хочешь ещё более явные иконки (требует Nerd Font):
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = "󰋽 " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
